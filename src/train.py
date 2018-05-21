c875b6b517e8251b1ccd56eea27c9722ed790368
takeuchi <takeuchi@nlp.ist.i.kyoto-u.ac.jp>
2018-05-21 18:07:17 +0900

    separate data to label and document

--- src/train.py
+++ src/train.py
@@ -3,11 +3,14 @@ from gensim.models import KeyedVectors
 
 
 def load_data(path_to_data):
-    data = []
+    xs = []
+    ys = []
     with open(path_to_data) as raw_data:
         for line in raw_data:
-            data.append((line.strip().split(',')))
-    return data
+            y,x = line.strip().split(',')
+            xs.append(x)
+            ys.append(y)
+    return xs, ys
 
 
 def main():
@@ -16,9 +19,10 @@ def main():
     PATH_TO_TRAIN = os.path.join(DATA_DIR, 'train.csv')
     PATH_TO_TEST = os.path.join(DATA_DIR, 'test.csv')
     PATH_TO_WE = '/share/data/word2vec/2016.08.02/w2v.midasi.256.100K.bin'
-    train = load_data(PATH_TO_TRAIN)
-    test = load_data(PATH_TO_TEST)
+    train_x, train_y = load_data(PATH_TO_TRAIN)
+    test_x, test_y = load_data(PATH_TO_TEST)
     word_vectors = KeyedVectors.load_word2vec_format(PATH_TO_WE, binary=True)
+    word2index = {}
     for index, word in enumerate(word_vectors.index2word):
         word2index[word] = index
     from IPython import embed;
