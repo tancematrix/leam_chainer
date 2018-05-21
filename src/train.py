aa955ae3444bbbfd1fbdcded7a9437bc5b678d6c
takeuchi <takeuchi@nlp.ist.i.kyoto-u.ac.jp>
2018-05-21 17:58:05 +0900

    update to load word2vec

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 import os
+from gensim.models import KeyedVectors
 
 
 def load_data(path_to_data):
@@ -14,8 +15,14 @@ def main():
     DATA_DIR = '/baobab/kiyomaru/2018-shinjin/jumanpp.midasi'
     PATH_TO_TRAIN = os.path.join(DATA_DIR, 'train.csv')
     PATH_TO_TEST = os.path.join(DATA_DIR, 'test.csv')
+    PATH_TO_WE = '/share/data/word2vec/2016.08.02/w2v.midasi.256.100K.bin'
     train = load_data(PATH_TO_TRAIN)
     test = load_data(PATH_TO_TEST)
+    word_vectors = KeyedVectors.load_word2vec_format(PATH_TO_WE, binary=True)
+    for index, word in enumerate(word_vectors.index2word):
+        word2index[word] = index
+    from IPython import embed;
+    embed()
 
     # convert document to ids
 
